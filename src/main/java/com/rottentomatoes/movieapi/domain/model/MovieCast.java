253a4f621e4757e0225e4e536982390bed56cf31
Peter Lau <peter.lau@flixster-inc.com>
2015-10-01 17:26:12 -0700

    Fix rename typos

--- src/main/java/com/rottentomatoes/movieapi/domain/model/MovieCast.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/model/MovieCast.java
@@ -21,10 +21,11 @@ import java.util.List;
 import io.katharsis.resource.annotations.JsonApiId;
 import io.katharsis.resource.annotations.JsonApiResource;
 import io.katharsis.resource.annotations.JsonApiToOne;
+import com.rottentomatoes.movieapi.domain.model.Person;
 import lombok.Getter;
 import lombok.Setter;
 
-@JsonApiResource(type = "castPerson")
+@JsonApiResource(type = "movieCast")
 @Getter
 @Setter
 public class MovieCast {
