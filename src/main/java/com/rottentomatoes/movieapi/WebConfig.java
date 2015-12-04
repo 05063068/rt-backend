1120f0dfb257a532626f918fba6af638aa9d6091
patrick <patrick.kamowski@flixster-inc.com>
2015-12-04 15:31:39 -0800

    Adding video clips.

--- src/main/java/com/rottentomatoes/movieapi/WebConfig.java
+++ src/main/java/com/rottentomatoes/movieapi/WebConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 import com.rottentomatoes.movieapi.filter.CatalogApiKatharsisFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Declares Katharsis Filter as a Bean in this configuration class.
@@ -19,6 +22,16 @@ public class WebConfig {
     public Filter catalogApiKatharsisFilter() {
         CatalogApiKatharsisFilter filter = new CatalogApiKatharsisFilter();
         return filter;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/movie/**");
+            }
+        };
     }
 
 }
---
+++ src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieToVideoClipRepository.java
@@ -0,0 +1,54 @@
+package com.rottentomatoes.movieapi.domain.repository;
+
+import com.rottentomatoes.movieapi.domain.model.Movie;
+import com.rottentomatoes.movieapi.domain.model.VideoClip;
+import io.katharsis.queryParams.RequestParams;
+import io.katharsis.repository.RelationshipRepository;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Component
+public class MovieToVideoClipRepository implements RelationshipRepository<Movie, String, VideoClip, String> {
+    @Autowired
+    private SqlSession sqlSession;
+
+    @Override
+    public void setRelation(Movie movie, String s, String s2) {
+
+    }
+
+    @Override
+    public void setRelations(Movie movie, Iterable<String> iterable, String s) {
+
+    }
+
+    @Override
+    public void addRelations(Movie movie, Iterable<String> iterable, String s) {
+
+    }
+
+    @Override
+    public void removeRelations(Movie movie, Iterable<String> iterable, String s) {
+
+    }
+
+    @Override
+    public VideoClip findOneTarget(String s, String s2, RequestParams requestParams) {
+        return null;
+    }
+
+    @Override
+    public Iterable<VideoClip> findManyTargets(String movieId, String fieldName, RequestParams requestParams) {
+        Map<String, Object> selectParams = new HashMap<>();
+        selectParams.put("movie_id", movieId);
+        selectParams.put("movie_id", movieId);
+
+        List<VideoClip> videoClipList = sqlSession.selectList("com.rottentomatoes.movieapi.mappers.VideoClipMapper.selectVideoClipsForMovie", selectParams);
+        return videoClipList;
+    }
+}
