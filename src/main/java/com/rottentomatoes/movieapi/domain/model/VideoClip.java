7b9fc93529567492b814d2f537425aa60a7c4331
patrick <patrick.kamowski@flixster-inc.com>
2015-12-11 13:35:50 -0800

    Added more data to the movie object.

--- src/main/java/com/rottentomatoes/movieapi/domain/model/VideoClip.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/model/VideoClip.java
@@ -9,6 +9,14 @@ import lombok.Setter;
 @Setter
 public class VideoClip extends AbstractModel{
 
+    protected Integer duration;
+    protected String title;
+    protected String thumbUrl;
+    protected String format;
+    protected String source;
     protected String url;
+    protected String language;
+    protected String status;
+    protected Integer favoriteCount;
 
 }
