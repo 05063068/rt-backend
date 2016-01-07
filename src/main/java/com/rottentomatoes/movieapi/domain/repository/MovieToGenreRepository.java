feb1fe1e6069c21467422dfdb41ab67c07714a87
Peter Lau <peter.lau@flixster-inc.com>
2016-01-07 12:36:53 -0800

    1) Break out genres as relationshiop 2) Implement tomatometer logic

---
+++ src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieToGenreRepository.java
@@ -0,0 +1,68 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.rottentomatoes.movieapi.domain.repository;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.rottentomatoes.movieapi.domain.model.Genre;
+import com.rottentomatoes.movieapi.domain.model.Image;
+import com.rottentomatoes.movieapi.domain.model.Movie;
+
+import io.katharsis.queryParams.RequestParams;
+import io.katharsis.repository.RelationshipRepository;
+
+@Component
+public class MovieToGenreRepository implements RelationshipRepository<Movie, String, Genre, String> {
+    @Autowired
+    private SqlSession sqlSession;
+    
+    @Override
+    public void addRelations(Movie arg0, Iterable<String> arg1, String arg2) {
+    }
+
+    @Override
+    public void removeRelations(Movie arg0, Iterable<String> arg1, String arg2) {
+    }
+
+    @Override
+    public void setRelation(Movie arg0, String arg1, String arg2) {
+    }
+    
+    @Override
+    public void setRelations(Movie arg0, Iterable<String> arg1, String arg2) {
+    }
+
+	@Override
+	public Genre findOneTarget(String id, String fieldName, RequestParams requestParams) {
+		return null;
+	}
+
+	@Override
+	public Iterable<Genre> findManyTargets(String movieId, String fieldName, RequestParams requestParams) {
+        Map<String, Object> selectParams = new HashMap<>();
+        selectParams.put("movie_id", movieId);
+		
+        List<Genre> genreList = sqlSession.selectList("com.rottentomatoes.movieapi.mappers.GenreMapper.selectGenresForMovie", selectParams);
+        return genreList;
+	}
+}
