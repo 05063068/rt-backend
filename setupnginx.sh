1ad0e29ec6101f96abe2fb85bcfd25e60028002a
Mark Painter <mark.painter@flixster-inc.com>
2016-03-18 10:37:05 -0700

    MC-222 running nginx on port 80 working, logstash working
    I found it problematic to remove apache entirely, because the eb
    deploy and status checks seem to rely on it.  However, I minimized
    the resources it uses, configured logstash to pull logs from
    nginx, and pulled apache completely out of the request processing.
    So, this seems like a good intermediate step to make available.

--- setupnginx.sh
+++ setupnginx.sh
@@ -12,14 +12,6 @@ for lb in $(aws elasticbeanstalk describe-environment-resources  --environment-n
        # setup nginx access
        echo setting up nginx access for "$lb"
        aws elb delete-load-balancer-listeners --load-balancer-name "$lb" --load-balancer-ports 80 --region "$REGION"
-       aws elb create-load-balancer-listeners --load-balancer-name "$lb" --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=81" --region "$REGION"
-       # add egress rule for nginx access
-       lbsecurity_group = $(aws elb describe-load-balancers --load-balancer-names "$lb" --region "$REGION" --query 'LoadBalancerDescriptions[0].SecurityGroups[0]' --output text)
-       aws ec2 authorize-security-group-egress --group-id "$lbsecurity_group" --ip-permissions '[{"IpProtocol": "tcp", "FromPort": 81, "ToPort": 81, "IpRanges": [{"CidrIp": "0.0.0.0/0"}]}]' --region "$REGION"
-       # add ingress rule for nginx access
-       launchcfgname=$(aws elasticbeanstalk describe-environment-resources  --environment-name "$BEANSTALK_ENVIRONMENT" --region "$REGION" --query EnvironmentResources.LaunchConfigurations[*].Name --output text)
-       lcsecurity=$(aws autoscaling describe-launch-configurations --launch-configuration-names "$launchcfgname" --region "$REGION" --query 'LaunchConfigurations[0].SecurityGroups[1]' --output text)
-       lc_security_id=$(aws ec2 describe-security-groups --group-names "$lcsecurity" --region "$REGION" --query 'SecurityGroups[0].GroupId' --output text)
-       aws ec2 authorize-security-group-ingress --group-id "$lc_security_id" --protocol tcp --port 81 --source-group "$lbsecurity_group" --region "$REGION"
+       aws elb create-load-balancer-listeners --load-balancer-name "$lb" --listeners "Protocol=HTTP,LoadBalancerPort=80,InstanceProtocol=HTTP,InstancePort=80" --region "$REGION"
 done
 
