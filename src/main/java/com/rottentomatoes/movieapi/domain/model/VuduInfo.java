207e87c5651eeffb2313dc3367d8810c71b54713
patrick <patrick.kamowski@flixster-inc.com>
2016-03-18 15:04:31 -0700

    MC-215 Using Java 8 LocalDate and ZoneDateTime instead of Date and DateTime.

--- src/main/java/com/rottentomatoes/movieapi/domain/model/VuduInfo.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/model/VuduInfo.java
@@ -4,18 +4,16 @@ import io.katharsis.resource.annotations.JsonApiResource;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.Date;
+import java.time.LocalDate;
 
 @JsonApiResource(type = "vuduInfo")
 @Getter
 @Setter
 public class VuduInfo extends AbstractModel {
-
     protected String format;
     protected String type;
     protected Float price;
     protected String currency;
-    protected Date startDate;
-    protected Date endDate;
-
+    protected LocalDate startDate;
+    protected LocalDate endDate;
 }
