30b40a42893c955386d36b127c29471dc59be404
Mark Painter <mark.painter@flixster-inc.com>
2016-04-01 16:33:08 -0700

    fixing omission from just merged pull request

--- setuphealthcheck.sh
+++ setuphealthcheck.sh
@@ -11,6 +11,6 @@ DEPLOY_HOME=$(dirname "$0")
 for lb in $(aws elasticbeanstalk describe-environment-resources  --environment-name "$BEANSTALK_ENVIRONMENT" --region "$REGION" --query 'EnvironmentResources.LoadBalancers[*].Name' --output text); do
        # setup health check
        echo setting up health check for "$lb"
-       aws elb configure-health-check --load-balancer-name "$lb" --health-check Target=HTTP:80/movie/9,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3
+       aws elb configure-health-check --load-balancer-name "$lb" --health-check Target=HTTP:80/movie/9,Interval=30,UnhealthyThreshold=2,HealthyThreshold=2,Timeout=3 --region "$REGION"
 done
 
