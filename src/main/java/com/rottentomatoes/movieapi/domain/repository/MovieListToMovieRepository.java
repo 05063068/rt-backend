61b38dc3a62881bb56ee6c87ec1d9f0c7b4b7012
U-Peter-THINK\Peter <peter.lau@flixster-inc.com>
2016-04-28 15:26:28 -0700

    Remove top-box-office-estimated as discussed.  top-box-office list should select and return estimated or actuals as needed.
    Temporarily send estimated box-office. See MC-254 .

--- src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieListToMovieRepository.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieListToMovieRepository.java
@@ -4,6 +4,7 @@ import static java.time.temporal.TemporalAdjusters.next;
 import static java.time.temporal.TemporalAdjusters.nextOrSame;
 import static java.time.temporal.TemporalAdjusters.previousOrSame;
 
+import java.io.Serializable;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.util.HashMap;
@@ -12,15 +13,18 @@ import java.util.Map;
 
 import org.springframework.stereotype.Component;
 
+import com.rottentomatoes.movieapi.domain.meta.RootMetaDataInformation;
 import com.rottentomatoes.movieapi.domain.model.Movie;
 import com.rottentomatoes.movieapi.domain.model.MovieList;
 
 import io.katharsis.queryParams.RequestParams;
+import io.katharsis.repository.MetaRepository;
 import io.katharsis.repository.RelationshipRepository;
 import io.katharsis.resource.exception.ResourceNotFoundException;
+import io.katharsis.response.MetaInformation;
 
 @Component
-public class MovieListToMovieRepository extends AbstractRepository implements RelationshipRepository<MovieList, String, Movie, String> {
+public class MovieListToMovieRepository extends AbstractRepository implements RelationshipRepository<MovieList, String, Movie, String>, MetaRepository<Movie> {
 
     @Override
     public void addRelations(MovieList arg0, Iterable<String> arg1, String arg2) {
@@ -56,22 +60,16 @@ public class MovieListToMovieRepository extends AbstractRepository implements Re
         selectParams.put("country", "us");
         
         switch (listId) {
-            case "top-box-office-estimated":
-                now = LocalDate.now();
-                start = now.with(previousOrSame(DayOfWeek.FRIDAY));
-                selectParams.put("startDate", start);
-
-                return sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectEstimatedTopBoxOfficeMovies", selectParams);
-
             case "top-box-office":
                 now = LocalDate.now();
                 start = now.with(previousOrSame(DayOfWeek.FRIDAY));
                 selectParams.put("startDate", start);
 
-                List<Movie> retval = sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectTopBoxOfficeMovies", selectParams);
-                System.out.println("size of returned list:" + retval.size());
-                return retval;
-                
+                /*
+                   List<Movie> retval = sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectTopBoxOfficeMovies", selectParams);
+                */  
+                return sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectEstimatedTopBoxOfficeMovies", selectParams);
+
             case "upcoming":
                 now = LocalDate.now();
                 end = now.plusMonths(3);
@@ -120,5 +118,81 @@ public class MovieListToMovieRepository extends AbstractRepository implements Re
             default:
                 throw new ResourceNotFoundException("Invalid list type");
         }
+    }
+    
+    @Override
+    public MetaInformation getMetaInformation(Object root, Iterable resources, RequestParams requestParams, Serializable castedResourceId) {
+        LocalDate now;
+        LocalDate start;
+        LocalDate end;
+        RootMetaDataInformation metaData = null;
+
+        Map<String, Object> selectParams = new HashMap<>();
+        selectParams.put("country", "us");
+
+        switch ((String) castedResourceId) {
+            case "top-box-office-estimated":
+                now = LocalDate.now();
+                start = now.with(previousOrSame(DayOfWeek.FRIDAY));
+
+                selectParams.put("startDate", start);
+
+                metaData = sqlSession.selectOne("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectEstimatedTopBoxOfficeMoviesCount", selectParams);
+                metaData.setRequestParams(requestParams);
+                break;
+
+            case "top-box-office":
+                now = LocalDate.now();
+                start = now.with(previousOrSame(DayOfWeek.FRIDAY));
+
+                selectParams.put("startDate", start);
+
+                metaData = sqlSession.selectOne("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectTopBoxOfficeMoviesCount", selectParams);
+                metaData.setRequestParams(requestParams);
+                break;
+
+            case "upcoming":
+                now = LocalDate.now();
+                end = now.plusMonths(3);
+
+                selectParams.put("startDate", now);
+                selectParams.put("endDate", end);
+
+                metaData = sqlSession.selectOne("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectUpcomingMoviesCount", selectParams);
+                metaData.setRequestParams(requestParams);
+                break;
+
+            case "opening":
+                now = LocalDate.now();
+                start = now.with(previousOrSame(DayOfWeek.MONDAY));
+                end = now.with(nextOrSame(DayOfWeek.SUNDAY));
+
+                selectParams.put("startDate", start);
+                selectParams.put("endDate", end);
+
+                metaData = sqlSession.selectOne("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectOpeningMoviesCount", selectParams);
+                metaData.setRequestParams(requestParams);
+                break;
+
+            case "top-rentals":
+                now = LocalDate.now();
+                end = now.with(next(DayOfWeek.SUNDAY));
+                //Start is 10 weeks in the past.
+                start = now.minusWeeks(10);
+
+                selectParams.put("startDate", start);
+                selectParams.put("endDate", end);
+
+                metaData = sqlSession.selectOne("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectTopRentalMoviesCount", selectParams);
+                metaData.setRequestParams(requestParams);
+                break;                
+                
+            case "new-on-dvd":
+            	metaData = new RootMetaDataInformation();
+            	
+            	metaData.setRequestParams(requestParams);                
+
+        }
+		return metaData;
     }
 }
