51f6d137ab7999bce3049f22b910ac0f4b114a96
Mark Painter <mark.painter@flixster-inc.com>
2016-04-28 10:39:37 -0700

    MC-250 movie list functions returning expected number of results
    For some reason including the usual movie select clause in the sql
    associated with these apis together with a limit clause resulted in
    not all results being returned (a mybatis bug??).  Anyway using a
    simpler clause works.
    
    MC-250 responding to review feedback
    
    MC-250 second round of review feedback

--- src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieListToMovieRepository.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieListToMovieRepository.java
@@ -1,5 +1,11 @@
 package com.rottentomatoes.movieapi.domain.repository;
 
+import static java.time.temporal.TemporalAdjusters.next;
+import static java.time.temporal.TemporalAdjusters.nextOrSame;
+import static java.time.temporal.TemporalAdjusters.previousOrSame;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +17,7 @@ import com.rottentomatoes.movieapi.domain.model.MovieList;
 
 import io.katharsis.queryParams.RequestParams;
 import io.katharsis.repository.RelationshipRepository;
+import io.katharsis.resource.exception.ResourceNotFoundException;
 
 @Component
 public class MovieListToMovieRepository extends AbstractRepository implements RelationshipRepository<MovieList, String, Movie, String> {
@@ -37,11 +44,81 @@ public class MovieListToMovieRepository extends AbstractRepository implements Re
     }
 
     @Override
-    public Iterable<Movie> findManyTargets(String movieId, String fieldName, RequestParams requestParams) {
+    public Iterable<Movie> findManyTargets(String listId, String fieldName, RequestParams requestParams) {
         Map<String, Object> selectParams = new HashMap<>();
-        selectParams.put("limit", getLimit(fieldName, requestParams));
 
-        List<Movie> movieList = sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectBoxOfficeMovies", selectParams);
-        return movieList;
+        LocalDate now;
+        LocalDate start;
+        LocalDate end;
+
+        selectParams.put("limit", getLimit("", requestParams));
+        selectParams.put("offset", getOffset("", requestParams));
+        selectParams.put("country", "us");
+        
+        switch (listId) {
+            case "top-box-office-estimated":
+                now = LocalDate.now();
+                start = now.with(previousOrSame(DayOfWeek.FRIDAY));
+                selectParams.put("startDate", start);
+
+                return sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectEstimatedTopBoxOfficeMovies", selectParams);
+
+            case "top-box-office":
+                now = LocalDate.now();
+                start = now.with(previousOrSame(DayOfWeek.FRIDAY));
+                selectParams.put("startDate", start);
+
+                List<Movie> retval = sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectTopBoxOfficeMovies", selectParams);
+                System.out.println("size of returned list:" + retval.size());
+                return retval;
+                
+            case "upcoming":
+                now = LocalDate.now();
+                end = now.plusMonths(3);
+
+                selectParams.put("startDate", now);
+                selectParams.put("endDate", end);
+
+                return sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectUpcomingMovies", selectParams);
+
+            case "opening":
+                now = LocalDate.now();
+                start = now.with(previousOrSame(DayOfWeek.MONDAY));
+                end = now.with(nextOrSame(DayOfWeek.SUNDAY));
+
+                selectParams.put("startDate", start);
+                selectParams.put("endDate", end);
+
+                return sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectOpeningMovies", selectParams);
+
+            case "top-rentals":
+                now = LocalDate.now();
+
+                end = now.with(next(DayOfWeek.SUNDAY));
+                //Start is 10 weeks in the past.
+                start = now.minusWeeks(10);
+
+                selectParams.put("startDate", start);
+                selectParams.put("endDate", end);
+
+                return sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectTopRentalMovies", selectParams);
+
+            case "new-on-dvd":
+                // TODO: Need to implement this
+                now = LocalDate.now();
+
+                end = now.with(next(DayOfWeek.SUNDAY));
+                //Start is 10 weeks in the past.
+                start = now.minusWeeks(10);
+
+                selectParams.put("startDate", start);
+                selectParams.put("endDate", end);
+
+                return sqlSession.selectList("com.rottentomatoes.movieapi.mappers.MovieListMapper.selectTopRentalMovies", selectParams);
+
+
+            default:
+                throw new ResourceNotFoundException("Invalid list type");
+        }
     }
 }
