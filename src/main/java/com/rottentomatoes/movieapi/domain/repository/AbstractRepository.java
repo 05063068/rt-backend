43289828c00738b16ccac59884cd349b73de0916
Mark W Painter <mark1@mpainter-mac.local>
2016-09-20 10:08:48 -0700

    EMS-456 removed mybatis and tested locally

--- src/main/java/com/rottentomatoes/movieapi/domain/repository/AbstractRepository.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/repository/AbstractRepository.java
@@ -4,9 +4,7 @@ import java.io.Serializable;
 import java.util.Map;
 
 import com.rottentomatoes.movieapi.enums.Country;
-import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 import io.katharsis.queryParams.PaginationKeys;
@@ -14,14 +12,7 @@ import io.katharsis.queryParams.RequestParams;
 
 @Component
 public class AbstractRepository implements Serializable {
-    @Autowired
-    @Qualifier("sqlSession")
-    protected SqlSession sqlSession;
 
-    @Autowired
-    @Qualifier("talkSession")
-    protected SqlSession talkSession;
-    
     @Autowired
     protected PreEmsConfig preEmsConfig;
 
