25ad9cfd538a5d7618cf72350b66b955b32a2900
patrick <patrick.kamowski@flixster-inc.com>
2016-10-21 16:21:37 -0700

    Fixed search total count.

--- src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieRepository.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/repository/MovieRepository.java
@@ -11,6 +11,7 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.databind.node.IntNode;
 import com.rottentomatoes.movieapi.domain.meta.RootMetaDataInformation;
 
 import com.rottentomatoes.movieapi.search.SearchQuery;
@@ -26,8 +27,6 @@ import io.katharsis.queryParams.RequestParams;
 import io.katharsis.repository.ResourceRepository;
 
 import com.rottentomatoes.movieapi.utils.RepositoryUtils;
-
-import static com.rottentomatoes.movieapi.utils.RepositoryUtils.getCountry;
 
 @SuppressWarnings("rawtypes")
 @Component
@@ -60,12 +59,8 @@ public class MovieRepository extends AbstractRepository implements ResourceRepos
         List<Long> movieIds = new ArrayList<>();
 
         if (requestParams.getFilters() != null && requestParams.getFilters().get("search") != null) {
-
             if(requestParams.getFilters().get("search") instanceof Map){
-                Map<String, Object> searchObj = (Map<String, Object>) requestParams.getFilters().get("search");
-
-                SearchQuery q = new SearchQuery("movies", searchObj);
-                JsonNode json = q.execute();
+                JsonNode json = callSearchService(requestParams);
                 ArrayNode resultArr = (ArrayNode) json.path("results");
                 movieIds = new ArrayList<>();
                 for (JsonNode movie : resultArr) {
@@ -82,11 +77,10 @@ public class MovieRepository extends AbstractRepository implements ResourceRepos
         selectParams.put("country", getCountry(requestParams).getCountryCode());
         if(movieIds.size() > 0) {
             movies = (List<Movie>) preEmsClient.callPreEmsList(selectParams, "movie", null, TypeFactory.defaultInstance().constructCollectionType(List.class, Movie.class));
+            return movies;
+        } else {
+            return null;
         }
-        else{
-            movies = new ArrayList<>();
-        }
-        return movies;
     }
 
     @Override
@@ -98,6 +92,22 @@ public class MovieRepository extends AbstractRepository implements ResourceRepos
     public MetaInformation getMetaInformation(Object root, Iterable resources, RequestParams requestParams, Serializable castedResourceId) {
         RootMetaDataInformation metaData = new RootMetaDataInformation();
         metaData.setRequestParams(requestParams);
+
+        if (requestParams.getFilters() != null && requestParams.getFilters().get("search") != null) {
+            if(requestParams.getFilters().get("search") instanceof Map){
+                JsonNode json = callSearchService(requestParams);
+                IntNode found = (IntNode) json.path("found");
+                metaData.setTotalCount(Integer.parseInt(found.toString()));
+            }
+        }
         return metaData;
+    }
+
+    private JsonNode callSearchService(RequestParams requestParams) {
+            Map<String, Object> searchObj = (Map<String, Object>) requestParams.getFilters().get("search");
+            SearchQuery q = new SearchQuery("movies", searchObj);
+            JsonNode json = q.execute();
+
+        return json;
     }
 }
