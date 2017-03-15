ff0f29bdbc9c717f24d644d1049ad02329cdec0b
Harry Zhu <harry.zhu@flixster-inc.com>
2017-03-14 18:01:14 -0700

    Move ApiCalls out of ems package since they are not at the service level

--- src/main/java/com/rottentomatoes/movieapi/domain/model/apicalldelegators/MovieListToMovieAllBoxOfficeApiCall.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/model/apicalldelegators/MovieListToMovieAllBoxOfficeApiCall.java
@@ -2,41 +2,39 @@
  * Flixster Inc. Copyright (c) 2017. All Rights Reserved.
  */
 
-package com.rottentomatoes.movieapi.domain.model.apicalldelegators.ems;
+package com.rottentomatoes.movieapi.domain.model.apicalldelegators;
 
 import java.util.List;
 
 import org.springframework.core.env.Environment;
 
 import com.rottentomatoes.movieapi.domain.model.Movie;
-import com.rottentomatoes.movieapi.domain.model.apicalldelegators.AbstractApiCall;
 import com.rottentomatoes.movieapi.domain.model.clients.Client;
-import com.rottentomatoes.movieapi.domain.model.requests.ems.EmsOpeningMoviesRequest;
+import com.rottentomatoes.movieapi.domain.model.requests.ems.EmsBoxOfficeRequest;
 import com.rottentomatoes.movieapi.domain.model.responses.ems.EmsMovieListResponse;
 import com.rottentomatoes.movieapi.domain.model.responses.ems.EmsToRottenTomatoesMovieConverter;
+import com.rottentomatoes.movieapi.domain.repository.movie.MovieListToMovieRepository;
 
 import io.katharsis.queryParams.RequestParams;
 
 /**
- * API implementation for the {@link com.rottentomatoes.movieapi.domain.repository.movie.MovieListToMovieRepository}
- * 'opening' endpoint
+ * API implementation for the {@link MovieListToMovieRepository} 'all-box-office' endpoint
  * 
  * @author harry
  */
-public class MovieListToMovieOpeningApiCall extends AbstractApiCall {
+public class MovieListToMovieAllBoxOfficeApiCall extends AbstractApiCall {
 
-    public MovieListToMovieOpeningApiCall(final Environment environment, final String fieldName,
-            final RequestParams requestParams) {
+    public MovieListToMovieAllBoxOfficeApiCall(final Environment environment,
+            final String fieldName, final RequestParams requestParams) {
         super(environment, fieldName, requestParams);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<Movie> process() {
-        EmsOpeningMoviesRequest request = new EmsOpeningMoviesRequest(environment, fieldName,
+        EmsBoxOfficeRequest request = new EmsBoxOfficeRequest(environment, fieldName,
                 requestParams);
         EmsMovieListResponse response = new EmsMovieListResponse(Client.makeApiCall(request));
         return EmsToRottenTomatoesMovieConverter.prepareMovies(response.getEmsMovies());
     }
-
 }
--- src/main/java/com/rottentomatoes/movieapi/domain/model/apicalldelegators/MovieListToMovieOpeningApiCall.java
+++ src/main/java/com/rottentomatoes/movieapi/domain/model/apicalldelegators/MovieListToMovieOpeningApiCall.java
@@ -2,40 +2,40 @@
  * Flixster Inc. Copyright (c) 2017. All Rights Reserved.
  */
 
-package com.rottentomatoes.movieapi.domain.model.apicalldelegators.ems;
+package com.rottentomatoes.movieapi.domain.model.apicalldelegators;
 
 import java.util.List;
 
 import org.springframework.core.env.Environment;
 
 import com.rottentomatoes.movieapi.domain.model.Movie;
-import com.rottentomatoes.movieapi.domain.model.apicalldelegators.AbstractApiCall;
 import com.rottentomatoes.movieapi.domain.model.clients.Client;
-import com.rottentomatoes.movieapi.domain.model.requests.ems.EmsBoxOfficeRequest;
+import com.rottentomatoes.movieapi.domain.model.requests.ems.EmsOpeningMoviesRequest;
 import com.rottentomatoes.movieapi.domain.model.responses.ems.EmsMovieListResponse;
 import com.rottentomatoes.movieapi.domain.model.responses.ems.EmsToRottenTomatoesMovieConverter;
+import com.rottentomatoes.movieapi.domain.repository.movie.MovieListToMovieRepository;
 
 import io.katharsis.queryParams.RequestParams;
 
 /**
- * API implementation for the {@link com.rottentomatoes.movieapi.domain.repository.movie.MovieListToMovieRepository}
- * 'top-box-office' endpoint
+ * API implementation for the {@link MovieListToMovieRepository} 'opening' endpoint
  * 
  * @author harry
  */
-public class MovieListToMovieTopBoxOfficeApiCall extends AbstractApiCall {
+public class MovieListToMovieOpeningApiCall extends AbstractApiCall {
 
-    public MovieListToMovieTopBoxOfficeApiCall(final Environment environment,
-            final String fieldName, final RequestParams requestParams) {
+    public MovieListToMovieOpeningApiCall(final Environment environment, final String fieldName,
+            final RequestParams requestParams) {
         super(environment, fieldName, requestParams);
     }
 
     @SuppressWarnings("unchecked")
     @Override
     public List<Movie> process() {
-        EmsBoxOfficeRequest request = new EmsBoxOfficeRequest(environment, fieldName,
+        EmsOpeningMoviesRequest request = new EmsOpeningMoviesRequest(environment, fieldName,
                 requestParams);
         EmsMovieListResponse response = new EmsMovieListResponse(Client.makeApiCall(request));
         return EmsToRottenTomatoesMovieConverter.prepareMovies(response.getEmsMovies());
     }
+
 }
